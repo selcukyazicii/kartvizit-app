3dc378463d3dce27347f7d9b6f9641bebefcce35
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-08-01 17:53:23 +0300

    ...

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatCardModule} from '@angular/material/card';
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
 
@@ -11,7 +11,8 @@ import { CardsComponent } from './cards.component';
   ],
   imports: [
     CommonModule,
-    CardsRoutingModule
+    CardsRoutingModule,
+    MatCardModule
   ]
 })
 export class CardsModule { }
