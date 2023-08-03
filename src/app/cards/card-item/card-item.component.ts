0a973289a54ee77192f2ade76e3bf6fceff0e958
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-08-03 09:46:39 +0300

    card interface added

--- src/app/cards/card-item/card-item.component.ts
+++ src/app/cards/card-item/card-item.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit ,Input} from '@angular/core';
+import { Card } from 'src/app/models/card';
 
 @Component({
   selector: 'app-card-item',
@@ -6,7 +7,7 @@ import { Component,OnInit ,Input} from '@angular/core';
   styleUrls: ['./card-item.component.scss']
 })
 export class CardItemComponent  implements OnInit{
-  @Input() cardItem:any;
+  @Input() cardItem!:Card
   constructor(){}
   ngOnInit():void{
   }
